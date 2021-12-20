4669ac664579b929e518549f69dd5ed7b431a964
Levi <3573897471@qq.com>
2021-12-20 14:45:43 +0800

    Mac in 21/12/20 FinalWork Bag2

--- FinalWork/Bag2/BagQueue.cpp
+++ FinalWork/Bag2/BagQueue.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "BagQueue.h"
+
+template<class T>
+BagQueue<T>::BagQueue(int Size) :Bag<T>(Size){}
+
+template<class T>
+BagQueue<T>::~BagQueue<T>() {}
--- FinalWork/Bag2/BagQueue.h
+++ FinalWork/Bag2/BagQueue.h
@@ -4,5 +4,12 @@
 
 #ifndef HOMEWORK_BAGQUEUE_H
 #define HOMEWORK_BAGQUEUE_H
+#include "Bag.h"
 
+template<class T>
+class BagQueue :public Bag<T> {
+public:
+    BagQueue(int Size = DaufaultSize);
+    ~BagQueue();
+};
 #endif //HOMEWORK_BAGQUEUE_H
